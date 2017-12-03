50d7c11e32a08da98a1857a76154247d51ae23e6
zhangoose <hi.zhalex@Gmail.com>
2017-12-03 13:21:29 -0500

    Adding firebase credz

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+var config = {
+    apiKey: "AIzaSyDPb7jVMTKUzq_9v_iDw-Rd0Upz8HMZi1g",
+    authDomain: "vuenyc-speakers.firebaseapp.com",
+    databaseURL: "https://vuenyc-speakers.firebaseio.com",
+    projectId: "vuenyc-speakers",
+    storageBucket: "",
+    messagingSenderId: "291569742721"
+};
